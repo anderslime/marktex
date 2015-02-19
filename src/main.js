141915b6acdd08bc1ba0a6798e09047aef703b7a
Andreas Toftegaard <toftegaard.andreas@gmail.com>
2015-02-19 23:37:12 +0100

    heroku grunt

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-var sharejsace = require('./components/sharejs/ace.js');
+var sharejsace = require('./components/sharejs/ace');
 
 var main = require('./controllers/main');
-var mathjaxservice = require('./services/mathjax.js');
+var mathjaxservice = require('./services/mathjax');
 
-var texlist = require('./directives/texList.js');
-var texloader = require('./directives/texLoader.js');
-var texstateashtml = require('./directives/texStateAsHtml.js');
-var texmarkdown = require('./directives/texMarkdown.js');
+var texlist = require('./directives/texList');
+var texloader = require('./directives/texLoader');
+var texstateashtml = require('./directives/texStateAsHtml');
+var texmarkdown = require('./directives/texMarkdown');
