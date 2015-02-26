a4066458b8db128d046612b9d9745402e62a291d
Andreas Toftegaard <toftegaard.andreas@gmail.com>
2015-02-26 14:44:21 +0100

    nice typesetting af mathjax

--- src/directives/texMarkdown.js
+++ src/directives/texMarkdown.js
@@ -5,18 +5,27 @@ texapp.directive('texMarkdown', ['$rootScope', '$compile', 'mathjaxservice', fun
 	return {
 		restrict: "A",
         scope: { 'texMarkdown': '=' },
+        template: '<div class="document"></div><div class="document-preview"></div>',
         link: function(scope, element, attrs) {
 			scope.$watch('texMarkdown', function(newVal, oldVal) {
 				if(timeout)
 					clearTimeout(timeout);
 
-				element.html(mathjaxservice.markdown(newVal));
+				var pdocElem = element.find('.document-preview');
+				var docElem = element.find('.document');
+
+				pdocElem.html(mathjaxservice.markdown(newVal));
 			    $compile(element.contents())(scope);
-			    $rootScope.$broadcast('jax-preprocess');
+			    $rootScope.$broadcast('jax-preprocess', pdocElem);
 			    
 				timeout = setTimeout(function(){
-					$rootScope.$broadcast('jax-typeset');
-				}, 500);
+					$rootScope.$broadcast('jax-typeset', pdocElem);
+				}, 300);
+
+				$rootScope.$on('jax-typeset-done', function(e, args){
+					docElem.html(pdocElem.html());
+					$rootScope.$apply();
+				});
 				
 			});
 	    }
--- src/services/mathjax.js
+++ src/services/mathjax.js
@@ -4,12 +4,12 @@ texapp.factory('mathjaxservice', ['$sanitize', 'markdownConverter', '$rootScope'
 	var sTimeout, typesetting = false;
 
 	$rootScope.$on('jax-preprocess', function(e, args){
-		MathJax.Hub.Queue(['PreProcess', MathJax.Hub, $('.document')[0]]);
+		MathJax.Hub.Queue(['PreProcess', MathJax.Hub, args[0]]);
 	});
 
 	$rootScope.$on('jax-typeset', function(e, args){
 		typesetting = true;
-		MathJax.Hub.Queue(['Typeset', MathJax.Hub, $('.document')[0]]);
+		MathJax.Hub.Queue(['Typeset', MathJax.Hub, args[0]]);
 		MathJax.Hub.Queue(function(){
 			typesetting = false;
 			$rootScope.$broadcast('jax-typeset-done');
@@ -21,17 +21,13 @@ texapp.factory('mathjaxservice', ['$sanitize', 'markdownConverter', '$rootScope'
 		return value.replace(/\\#/g,'\\\\#');
 	}
 
-	function markdown(d){
-		if(d === undefined || d.length === 0)
-			return '';
-
-		//return markdowned html
-		return $sanitize(markdownConverter.makeHtml(replaceSlashes(d)));
-	}
-
 	return {
 		markdown: function(d){
-			return markdown(d);
+			if(d === undefined || d.length === 0)
+				return '';
+
+			//return markdowned html
+			return $sanitize(markdownConverter.makeHtml(replaceSlashes(d)));
 		}
 	};
 }]);
